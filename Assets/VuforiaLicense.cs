1c4be28f319eab45a35303f9789a5ebf6f99de19
PaniniRS <bubemanchester@live.com>
2025-06-02 00:14:56 +0200

    Added UI Custom Assets

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QWVteXpobi8vLy8vQUFBQm1WSjBUdFlmazBSbmt1M3JYQytIQ2RrbTYwVm8yOTJMcVhSS2dHM3JORTRVSVZpRHpiTm9hWXc0bVVMMzlpUlgzYnJFTEZJM2lldVNpL0NwVkg3N29oU3Q2RW81NHZtenlkSzZPd1lGZXVFTzdtYWhYNUlKOXpzek1jd2JjMHZacDJzT09CNC8raDMwTEE0aitUSWlxUXQ3NCtHSFk1RGRNZERzYmhvbm5zM3kySm50Tm9CTGM2MGsreWIxaTYzOGRlaFlWUC9CN0JrTGlxaGpSdDIva0xSY1hCL2o5QUdjN1NQbjFTVkN3b0lpZkNBWkFIcGlmSlFhUlFtdFBpazM1Y3RJZGJ1c3VGdnBxaEl3QTZZc1hPanJxQ3Z1OHdPQmJMdkd4NXZjWERGTGJLL3JYMWZYRCtZSkJRVnp4QWNacFdQdElBaW5rK0ZPWnRwVEJCODBBbU4vSnpDaUtpb1htN280OGwrUmJsWkQ=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
